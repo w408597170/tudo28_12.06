9fb50c22286264af0890aaad6642d3cf9f4bb076
Simon <408597170@qq.com>
2018-11-24 00:53:01 +0800

    项目初始化, 基本的三个页面.

--- handlers/main.py
+++ handlers/main.py
@@ -1,21 +1,46 @@
 import tornado.web
 import glob
-import os
-import photo
+
+from pycket.session import SessionMixin
+
+from utils.photo import save_upload, make_thumb
 
 
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
-        self.render('index.html')
+class AuthBaseHandler(tornado.web.RequestHandler, SessionMixin):
+    """
+    基础的认证 handler
+    """
+    def get_current_user(self):
+        return self.session.get('simon_user_info', None)
 
 
-class ExploreHandler(tornado.web.RequestHandler):
+
+
+class IndexHandler(AuthBaseHandler):
+    """
+    首页,显示用户关注的图片流.
+    """
+
+    @tornado.web.authenticated
     def get(self, *args, **kwargs):
         names = glob.glob('static/uploads/*.jpg')
+        self.render('index.html',names=names)
+
+
+class ExploreHandler(AuthBaseHandler):
+    """
+    发现页,最新上传的图片.
+    """
+    @tornado.web.authenticated
+    def get(self, *args, **kwargs):
+        names = glob.glob('static/uploads/thumbs/*.jpg')
         self.render('explore.html',names=names)
 
 
 class PostHandler(tornado.web.RequestHandler):
+    """
+    单个图片的详情页.
+    """
     def get(self, *args, **kwargs):
         self.render('post.html',post_id = kwargs['post_id'])
         
@@ -32,6 +57,6 @@ class UploadHandler(tornado.web.RequestHandler):
             content = upload['body']
             # with open('static/uploads/{}'.format(name), 'wb') as f:
             #     f.write(content)
-            photo.save_upload(name,content)
-            photo.make_thumb(name,(200, 200))
+            save_upload(name,content)
+            make_thumb(name,(200, 200))
         self.write('upload done')
