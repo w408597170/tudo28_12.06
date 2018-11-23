109bdb1afc21a2ef395a26690afde8f88f5f9c61
Simon <408597170@qq.com>
2018-11-24 00:52:40 +0800

    项目初始化, 基本的三个页面.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tornado.web  #web服务器
 import tornado.options  #命令行解析模块,让模块定义自己的选项.
 from tornado.options import define,options
 
-from handlers import main
+from handlers import main, auth
 
 define('port',default=8083,help='run port',type=int)
 
@@ -15,11 +15,30 @@ class Application(tornado.web.Application):
             (r'/explore', main.ExploreHandler),
             (r'/post/(?P<post_id>[0-9]+)', main.PostHandler),
             (r'/upload', main.UploadHandler),
+            (r'/login', auth.LoginHandler),
+            (r'/logout', auth.LogoutHandler),
+            (r'/signup', auth.SignupHandler),
         ]
         settings = dict(
             template_path = 'templates',    #设置模板路径.
             debug=True,  #开启调试模式,代码有修改的时候自动重启服务,生产者模式切记不能开启.
-            static_path='static'
+            static_path='static',
+            pycket={
+                'engine': 'redis',
+                'storage': {
+                    'host': 'localhost',
+                    'port': 6379,
+                    # 'password': '',
+                    'db_sessions': 5,  # redis db index
+                    'db_notifications': 11,
+                    'max_connections': 2 ** 30,
+                },
+                'cookies': {
+                    'expires_days': 30,
+                },
+            },
+            cookie_secret = 'skldjfskljfksld;jf',
+            login_url = '/login',
 
         )
 
